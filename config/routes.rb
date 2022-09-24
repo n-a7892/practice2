8be01a4dae2891198c32b86b697d7838cc767811
EC2 Default User <ec2-user@ip-172-31-5-114.ec2.internal>
2022-09-24 13:42:59 +0000

    deviseMVC作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+  sessions: "admin/sessions"
+}
+
+  devise_for :customers, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
   root to: "homes#top"
   get 'homes/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
