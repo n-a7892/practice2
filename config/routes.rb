8541bfd9af53a14e8b4c35c7da7af53c8b49de29
EC2 Default User <ec2-user@ip-172-31-5-114.ec2.internal>
2022-09-25 14:51:25 +0000

    Edit登録ログイン画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :admins, controllers: {
-  sessions: "admin/sessions"
+  sessions: "admin/sessions",
+  registrations: "admin/registrations"
 }
 
   devise_for :customers, controllers: {
   registrations: "public/registrations",
-  sessions: 'public/sessions'
+  sessions: "public/sessions"
 }
 
   root to: "homes#top"
