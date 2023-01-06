ad8d630e9effd83abc6c9b1958d527b2640c7984
EC2 Default User <ec2-user@ip-172-31-5-114.ec2.internal>
2023-01-06 10:06:25 +0000

    BigGenresコントローラ作成

---
+++ app/controllers/admin/big_genres_controller.rb
@@ -0,0 +1,29 @@
+class Admin::BigGenresController < ApplicationController
+  def index
+    @big_genre = BigGenre.new
+    @big_genres = BigGenre.all
+  end
+
+  def create
+    @big_genre = BigGenre.new(big_genre_params)
+    @big_genre.save
+    redirect_to admin_big_genres_path
+  end
+
+  def edit
+    @big_genre = BigGenre.find(params[:id])
+  end
+
+  def update
+    @big_genre = BigGenre.find(params[:id])
+    @big_genre.update(big_genre_params)
+    redirect_to admin_big_genres_path
+  end
+
+  private
+
+  def big_genre_params
+    params.require(:big_genre).permit(:name)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   namespace :admin do
     resources :messages, only: [:new, :create]
+    resources :big_genres, only: [:index, :create, :edit, :update]
+    resources :small_genres, only: [:index, :create, :edit, :update]
+
   end
+
   namespace :public do
     resources :messages, only: [:show, :index]
   end
+
   devise_for :admins, controllers: {
   sessions: "admin/sessions",
   registrations: "admin/registrations"
